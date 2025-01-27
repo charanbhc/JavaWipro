c97ceb300741bbc7ac22c608af2a86d4a88fe130
Haricharan Reddy <bharicharanreddy@gmail.com>
2025-01-27 11:52:48 +0530

    day 4

---
+++ ClassWork/Day4Jan27/Hello.java
@@ -0,0 +1,71 @@
+public class Hello {
+
+	//instance  variable  object creation is needed for 
+	
+	int i =10 ; // instance variabe  // instance  Heap memory   objects
+	static int  k =18; //static  //class area  method area   u need no to create any object 
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+   System.out.println(hello.i);
+      int j=15; //local variable  //stack
+      System.out.println(Hello.k);  // static  String organisation = "wipro"
+      
+      System.out.println(j);
+System.out.println("first changes in git ");
+System.out.println("Second  changes in git ");
+
+
+
+	}
+
+	
+
+}
+
+
+
+
+// https://github.com/chiru49/wiproDemo.git
+//   310  ls
+//   311  git status
+//   312  ls
+//   313  git log --online --decorate
+//   314  git branch
+//   315  cd wiproDemo
+//   316  git branch
+//   318  git status
+//   319  git status
+//   320  git add .
+//   321  git status
+//   322  git status
+//   323  git add .
+//   324  git status
+//   325  git commit -m "my first commit "
+//   326  git status
+//   327  git remote -v
+//   328  git branch
+//   329  git push origin main
+//   330  git log --online --decorate
+//   331  git log --oneline --decorate
+//   332  git show ca0b2fb
+//   333  git log
+//   334  got log --online
+//   335  clear
+//   336  git status
+//   337  git checkout -b mydevbranch
+//   338  git branch
+//   339  git branch
+//   340  git log --oneline --decorate
+//   341  git status
+//   342  git add .
+//   343  git status
+//   344  git commit -m "my second commit "
+//   345  git status
+//   346  git remote -v
+//   347  git push origin mydevbranch
+//   348  git branch
+//   349  git checkout main
+//   350  git branch
+//   351  git merge mydevbranch
+//   352  git push origin main
+//   353  history
